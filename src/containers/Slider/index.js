8b76a66da3f490fa3e8d434e33f3285a05d07180
Squaad6z <carneiromoreirasteven22@gmail.com>
2023-09-03 19:37:20 +0200

    Ajout variable slider, debogage image blanche

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -10,9 +10,10 @@ const Slider = () => {
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
     new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
   );
+  const eventSlide = data?.focus?.length
   const nextCard = () => {
     setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
+      () => setIndex(index < eventSlide -1 ? index + 1 : 0),
       5000
     );
   };
